2d896198026b3523548676ffc97c77695b5341d2
Nathan Rydel <nathanrydel@gmail.com>
2024-01-22 15:58:35 -0700

    updated main app structure

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DogList from "./DogList";
 import DogDetails from "./DogDetails";
+import NavBar from './NavBar';
 
 /** Main App component */
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route element= {<DogList />} path="/"/>
         <Route element= { <DogDetails />} path="/dogs/:name" />
